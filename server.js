af717f8ef5cf27a806551dfb00e5d26b58535629
Lucas <xolmirzayevotabek779@gmail.com>
2023-05-22 02:40:35 +0900

    loyihani express qismini shakllantirib olish

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const mongodb = require("mongodb");
+
+let db;
+const connectionString =
+  "mongodb+srv://lucasgenius95:W71qoOwF8I4w2kch@lucas.glhy18w.mongodb.net/Reja?retryWrites=true&w=majority";
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connnection succeed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is runing successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
+);
