465dd97ac90fafa6a1f0240dcf6d418566b96f9d
Lucas <xolmirzayevotabek779@gmail.com>
2023-05-25 23:59:47 +0900

    feat: dotenv & project configuration

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const http = require("http");
 const mongodb = require("mongodb");
 
-let db;
-const connectionString =
-  "mongodb+srv://lucasgenius95:W71qoOwF8I4w2kch@lucas.glhy18w.mongodb.net/Reja?retryWrites=true&w=majority";
+const connectionString = process.env.MONGO_URL;
 mongodb.connect(
   connectionString,
   {
@@ -17,7 +18,7 @@ mongodb.connect(
       module.exports = client;
       const app = require("./app");
       const server = http.createServer(app);
-      let PORT = 3000;
+      let PORT = process.env.PORT || 3003;
       server.listen(PORT, function () {
         console.log(
           `The server is runing successfully on port: ${PORT}, http://localhost:${PORT}`
