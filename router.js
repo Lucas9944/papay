9d3d696876d0763428476d5b152c1d9b667c7c79
Lucas <xolmirzayevotabek779@gmail.com>
2023-05-30 12:56:15 +0900

    feat: member controllerlarni qurish

--- router.js
+++ router.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const memberController = require("./controllers/memberController");
+// memberga dahildor routerlar
+router.get("/", memberController.home);
+router.post("/signup", memberController.signup);
+router.post("/login", memberController.login);
+router.get("/logout", memberController.logout);
 
-router.get("/", function(req, res) {
-    res.send("home sahifadasiz");
-});
-
+// boshqa routerlar
 router.get("/menu", (req, res) => {
   res.send("Menu sahifadasiz");
 });
-
 router.get("/community", (req, res) => {
   res.send("Jamiyat sahifadasiz");
 });
---
+++ train.js
@@ -0,0 +1,62 @@
+console.log("WELCOME TRAIN ARENA");
+const moment = require("moment");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /** CHALLENGE: MAX PROFIT */
+
+// function buyStock(stock_prices) {
+//   let kichikSon = stock_prices[0];
+//   let kattaSon = stock_prices[0];
+//   let indexPrices = 0;
+//   let realTime = moment();
+
+//   for (let i = 1; i < stock_prices.length; i++) {
+//     if (stock_prices[i] < kichikSon) {
+//       kichikSon = stock_prices[i];
+//       indexPrices = i;
+//     }
+
+//     if (stock_prices[i] > kattaSon) {
+//       kattaSon = stock_prices[i];
+      
+//     }
+//   }
+
+//   console.log(
+//     `${realTime.format(
+//       "YYYY-MM-DD"
+//     )} Aksiya bozoridagi eng arzon aksiya: ${kichikSon}`
+//   );
+//   console.log(
+//     `${realTime.format(
+//       "YYYY-MM-DD"
+//     )} Aksiya bozordagi eng qimmat aksiya: ${kattaSon}`
+//   );
+//   let maxProfit = kattaSon - kichikSon;
+//   console.log(`You should buy stock => index: ${indexPrices}, profit: ${maxProfit} `);
+// }
+
+// const prices = [2, 5, 3, 10];
+// buyStock(prices);
+
+
