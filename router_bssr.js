3c8205f9b409dd5f03218f5d924392ae7a945816
Lucas <xolmirzayevotabek779@gmail.com>
2023-06-06 01:35:13 +0900

    feat: develop Product controller logic

--- router_bssr.js
+++ router_bssr.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router_bssr = express.Router();
 const restaurantController = require("./controllers/restaurantController");
+const productController = require("./controllers/restaurantController");
 
 /*******************************
  *          REST EJS           *
@@ -14,8 +15,16 @@ router_bssr
   .get("/login", restaurantController.getLoginMyRestaurant)
   .post("/login", restaurantController.loginProcess);
 router_bssr.get("/logout", restaurantController.logout);
-router_bssr.get("/logout", restaurantController.checkSessions);
+router_bssr.get("/check-me", restaurantController.checkSessions);
 
-router_bssr.get("products/menu", restaurantController.getMyRestaurantData);
+router_bssr
+  .get("products/menu", restaurantController.getMyRestaurantData)
+  .post(
+    "/products/create",
+    restaurantController.validateAuthRestaurant,
+    productController.addNewProduct
+  );
+
+router_bssr.post("products/edit/:id", productController.updateChosenProduct);
 
 module.exports = router_bssr;
