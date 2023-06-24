a8c4de64d17d87ed0256819ae9b19d1d71713499
Lucas <xolmirzayevotabek779@gmail.com>
2023-06-24 15:19:50 +0900

    fix: complete development of allrestaurants business logic

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,4 +1,5 @@
 const assert = require("assert");
+const { shapeIntoMongooseObjectId } = require("../lib/config");
 const Definer = require("../lib/mistake");
 const MemberModel = require("../schema/member.model");
 
@@ -16,6 +17,25 @@ class Restaurant {
         .exec();
 
       assert(result, Definer.general_err1);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async updateRestaurantByAdminData(update_data) {
+    try {
+      const id = shapeIntoMongooseObjectId(update_data?.id);
+      const result = await this.memberModel
+        .findByIdAndUpdate({ _id: id }, update_data, {
+          runValidators: true,
+          lean: true,
+          returnDocument: "after",
+        })
+        .exec();
+
+
+      assert.ok(result, Definer.general_err1);
       return result;
     } catch (err) {
       throw err;
