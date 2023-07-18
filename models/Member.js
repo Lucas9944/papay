715b8ae9ee3637992b52229e5f3caf14fae7a297
Lucas <xolmirzayevotabek779@gmail.com>
2023-07-18 09:31:20 +0900

    feat: getChosenMember rest API development part one

--- models/Member.js
+++ models/Member.js
@@ -2,6 +2,8 @@ const MemberModel = require("../schema/member.model");
 const Definer = require("../lib/mistake");
 const assert = require("assert");
 const bcrypt = require("bcryptjs");
+const { shapeIntoMongooseObjectId } = require("../lib/config");
+const { log } = require("console");
 
 class Member {
   constructor() {
@@ -34,8 +36,9 @@ class Member {
     try {
       const member = await this.memberModel
         .findOne(
-          { mb_nick: input.mb_nick }, 
-          { mb_nick: 1, mb_password: 1, _id: 0})
+          { mb_nick: input.mb_nick },
+          { mb_nick: 1, mb_password: 1, _id: 0 }
+        )
         .exec();
 
       assert.ok(member, Definer.err_auth3);
@@ -46,10 +49,31 @@ class Member {
       );
       assert.ok(isMach, Definer.err_auth3);
 
-      return await this.memberModel
-      .findOne({ mb_nick: input.mb_nick })
-      .exec();
+      return await this.memberModel.findOne({ mb_nick: input.mb_nick }).exec();
       // console.log("member::::", member);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getChosenMemberData(member, id) {
+    try {
+      id = shapeIntoMongooseObjectId(id);
+      console.log("member:::", member)
+
+      if(member) {
+        // condition if not seen before
+      }
+
+      const result = await this.memberModel
+        .aggregate([
+          { $match: { _id: id, mb_status: "ACTIVE" } },
+          { $unset: "mb_password" },
+        ])
+        .exec();
+
+      assert.ok(result, Definer.general_err2);
+      return result[0];
     } catch (err) {
       throw err;
     }
